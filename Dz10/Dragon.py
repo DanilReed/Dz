c249718259c5e47852b832ae486b0495fadedd48
Damian <johndoe@example.com>
2023-06-10 21:50:35 +0300

    end1

--- Dz10/Dragon.py
+++ Dz10/Dragon.py
@@ -55,16 +55,3 @@ class Dragon:
     def __repr__(self):
         return f'{self.height}, {self.danger}, {self.color}'
 
-dr = Dragon(69, 5, "brown")
-dr1 = Dragon(69, 5, "gray")
-print(dr > dr1, dr != dr1, dr <= dr1)
-print(dr, dr1, sep="\n")
-print()
-
-dr.change_color("white")
-dr -= 23
-dr1 -= 2
-dr2 = dr + dr1
-print(dr, dr1, dr2, sep="\n")
-
-print(dr("Welcome"))
